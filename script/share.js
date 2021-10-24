04af0fe96cfc0b267ce0c85195bcb55d2e8b7418
little fish <i529561@gmail.com>
2021-10-24 13:47:33 +0800

    test

---
+++ script/share.js
@@ -0,0 +1,9 @@
+
+function redirect(path) {
+    window.location.href = "https://littlexfish.github.io/" + path
+}
+
+function relativeRedirect(path) {
+    window.location.href = window.location.href + path
+}
+
