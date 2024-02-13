7a00f58ebb9662a16d12ec81b83a73dfcd6639cf
little fish <i529561@gmail.com>
2024-02-13 12:07:15 +0800

    Make `open` command can open url, and fix some title problem

--- src/jsMain/kotlin/command/cmds/fs/Open.kt
+++ src/jsMain/kotlin/command/cmds/fs/Open.kt
@@ -18,23 +18,31 @@ class Open : Command(CommandType.FS) {
 			return 1
 		}
 		
+		val open = openedFile.last()
 		val type = if(pArg.has("h") || pArg.has("html")) {
 			"html"
 		}
 		else if(pArg.has("t") || pArg.has("text")) {
 			"text"
 		}
+		else if(open.matches("https?://([\\w.]+)(:\\d{1,5})?(/.*)?".toRegex())) {
+			"url"
+		}
 		else {
 			""
 		}
 		
-		val open = openedFile.last()
-		if(!FS.hasFile(open, env["PWD"])) {
+		if(type != "url" && !FS.hasFile(open, env["PWD"])) {
 			tunnel.pipeOutErrorTextTr("command.open.not_found", "file" to open)
 			return 1
 		}
 		val id = Application.startApp(FileViewer())
-		Application.sendMessage(id, "open", mapOf("file" to FS.getAbsolutePath(FS.getFile(open, relativeFrom = env["PWD"])), "type" to type))
+		if(type == "url") {
+			Application.sendMessage(id, "url", mapOf("url" to open))
+		}
+		else {
+			Application.sendMessage(id, "open", mapOf("file" to FS.getAbsolutePath(FS.getFile(open, relativeFrom = env["PWD"])), "type" to type))
+		}
 		return 0
 	}
 
